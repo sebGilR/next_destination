f280bcd0add6988258a76903d66f9c363f9f0289
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-18 14:13:11 -0500

    Include password length validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: {minimum: 3, maximum: 20}, uniqueness: true
+  validates :password, presence: true, length: {minimum: 5}
   has_secure_password
 
   has_many :favorites, dependent: :destroy
