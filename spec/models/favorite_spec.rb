61cca0397b56a35e82d141e186cc5eb5b1d1e57d
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-20 00:10:37 -0500

    Test favorite model

--- spec/models/favorite_spec.rb
+++ spec/models/favorite_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Favorite, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Returns false if the favorite already existed for the destination' do
+    create_destination
+    create_consumer_user
+    destId = Destination.last.id
+    Favorite.create(user_id: User.last.id, destination_id: destId)
+
+    fav = Favorite.new(user_id: User.last.id, destination_id: destId)
+    expect(fav.save).to be(false)
+  end
+
+  it 'Returns true if favorite didn\'t exist' do
+    create_destination
+    create_consumer_user
+    destId = Destination.last.id
+
+    fav = Favorite.new(user_id: User.last.id, destination_id: destId)
+    expect(fav.save).to be(true)
+  end
 end
