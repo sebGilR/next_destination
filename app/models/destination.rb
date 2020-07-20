1508202da25c561cbb43309fe5a3999169edbdc4
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-20 00:09:47 -0500

    Validate img_url presence

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,10 +2,9 @@ class Destination < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true
   validates :img_url, format: { with: /.+\.(gif|jpe?g|png)\z/,
-    message: 'only allows gif, jpeg, jpg or png formats.' }
+    message: 'only allows gif, jpeg, jpg or png formats.' }, presence: true
 
   has_many :favorites, dependent: :destroy
 
   scope :order_by_most_favorites, -> { order(favorites_count: :desc) }
-  scope :order_by_least_favorites, -> { order(favorites_count: :asc) }
 end
